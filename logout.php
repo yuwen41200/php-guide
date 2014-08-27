82abf38a76b90b88f7866d11d090403ce6e61d5f
yuwen41200 <yuwen41200@gmail.com>
2014-08-27 13:33:28 +0800

    Added Login/Logout

---
+++ logout.php
@@ -0,0 +1,16 @@
+<?php
+	if(isset($_POST['logout']) && $_POST['logout']=="true") {
+		unset($_SESSION['LoginUser']);
+		header('Location: index.php');
+		exit();
+	}
+?>
+<html>
+	<body>
+		<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
+			<p><?php echo $_SESSION['LoginUser']; ?></p>
+			<p><input type="submit" value="Logout"></p>
+			<input type="hidden" name="logout" value="true"/>
+		</form>
+	</body>
+</html>

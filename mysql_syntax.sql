7fd2be0859e734c99a153697378c84761a0c64f8
Yu-wen Pwu <yuwen41200@gmail.com>
2014-09-07 19:56:49 +0800

    Added Index, Unique, Foreign Key

--- mysql_syntax.sql
+++ mysql_syntax.sql
@@ -15,20 +15,29 @@ SHOW TABLES;
 
 -- CREATE TABLE Syntax
 CREATE TABLE `table` (
-	`column1` INT(20) NOT NULL AUTO_INCREMENT, 
-	`column2` VARCHAR(20) NOT NULL,
-	`column3` TEXT NOT NULL,
-	`column4` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (`column1`)
-) ENGINE = MyISAM DEFAULT CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
+	`column1` INT(20) NOT NULL AUTO_INCREMENT,
+	`column2` INT(20) NOT NULL,
+	`column3` INT(20) NOT NULL,
+	`column4` INT(20),
+	`column5` VARCHAR(20) NOT NULL,
+	`column6` TEXT NOT NULL,
+	`column7` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`column1`),
+	UNIQUE (`column2`),
+	INDEX (`column3`),
+	CONSTRAINT foreign_key_name FOREIGN KEY (`column4`) REFERENCES another_table (`primary_key`) ON UPDATE CASCADE ON DELETE RESTRICT
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
 
 -- DESCRIBE Syntax
 DESCRIBE `table`;
 
 -- ALTER TABLE Syntax
 ALTER TABLE `table`
-MODIFY `column2` VARCHAR(20) NOT NULL DEFAULT 'value',
-DROP `column3`;
+ADD `column1` INT(20) NOT NULL AFTER `another_column`,
+ADD PRIMARY KEY (`column2`),
+MODIFY `column3` VARCHAR(20) NOT NULL DEFAULT 'value',
+DROP `column4`,
+DROP FOREIGN KEY foreign_key_name;
 
 -- SELECT Syntax
 SELECT `column` FROM `table`
